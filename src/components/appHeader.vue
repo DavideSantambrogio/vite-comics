cf9b7900aa8d1026802f1ffdc189dbea3218189e
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2023-11-23 16:06:44 +0100

    Add header layout

---
+++ src/components/appHeader.vue
@@ -0,0 +1,61 @@
+<script>
+export default {
+  data() {
+    return {
+      navItems: [
+        {
+          title: "CHARACTERS",          
+        },
+        {
+          title: "COMICS",      
+        },
+        {
+          title: "MOVIES",          
+        },
+        {
+          title: "TV",          
+        },
+        {
+          title: "GAMES",          
+        },
+        {
+          title: "COLLECTIBLES",      
+        },
+        {
+          title: "VIDEOS",          
+        },
+        {
+          title: "FANS",          
+        },
+        {
+          title: "NEWS",          
+        },
+        {
+          title: "SHOP",      
+        },        
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <header>
+    <div class="logo">
+      <img src="../assets/img/dc-logo.png" alt="DC" />
+    </div>
+    <nav>
+      <ul>
+        <li v-for="item in navItems" >
+          <a href="">{{ item.title }}</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped lang="scss">
+
+
+
+</style>
