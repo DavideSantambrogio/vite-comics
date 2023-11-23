85a8033a1c43cb5196dc586d5dc6e663f5278f98
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2023-11-23 20:55:20 +0100

    add banner

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -2,6 +2,28 @@
 export default {
   data() {
     return {
+        callToActList: [
+        {
+          icon: "../src/assets/img/buy-comics-digital-comics.png",
+          name: "DIGITAL COMICS",            
+        },
+        {
+          icon: "../src/assets/img/buy-comics-merchandise.png",
+          name: "DC MERCHANDISE",            
+        },
+        {
+          icon: "../src/assets/img/buy-comics-subscriptions.png",
+          name: "SUBSCRITION",            
+        },
+        {
+          icon: "../src/assets/img/buy-comics-shop-locator.png",
+          name: "COMICS SHOP LOCATOR",            
+        },
+        {
+          icon: "../src/assets/img/buy-dc-power-visa.svg",
+          name: "DC POWER VISA",            
+        }
+      ]
       
     };
   },
@@ -9,19 +31,54 @@ export default {
 </script>
 
 <template>
-    <div class="banner">
-    
-    </div>
+    <section>
+        <div class="container"> 
+          <ul>
+            <li v-for="callToAct in callToActList">
+              <img :src=(callToAct.icon) :alt="`${callToAct.name}`">
+              <p>{{callToAct.name}}</p>
+            </li>
+          </ul>
+        </div>
+    </section>
 </template>
 
 <style scoped lang="scss">
 @use "../style/partials/mixin" as *;
 @use "../style/partials/variables" as *;
-.banner {
-    height: 120px;
-    background-color: $primary;
+
+section {
+  background-color: $primary;
 }
 
+li:last-child img{
+  width: 3rem;
+}
 
+img {
+   
+  height: 3rem;
+}
+
+ul {
+  display: flex;
+  align-items: center;
+  padding: 2rem;
+  justify-content: space-around;
+}
+
+li {
+  list-style-type: none;
+  display: flex;
+  color: white;
+  align-items: center;
+  gap: 0.5rem;
+  
+}
+
+p {
+  font-size: 0.7rem;
+}
 
 </style>
+
